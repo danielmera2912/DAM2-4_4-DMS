5ce8caff612fa9d307f63e896f009953ae6ee3a5
danielmera2912 <dmera@iesrafaelalberti.es>
2021-12-01 19:38:11 +0100

    1.5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,27 +3,29 @@ class Pila<T>(var lista: List<T>){
     private val listaMutable = lista.toMutableList()
     fun get()= listaMutable
     fun push(item: T) = listaMutable.add(item)
-    fun pop()= if (listaMutable.isNotEmpty()) listaMutable.removeAt(listaMutable.size) else null
+    fun pop()= if (listaMutable.isNotEmpty()) listaMutable.removeAt(listaMutable.size-1) else null
     fun tope()=listaMutable.lastOrNull()
     fun vacia() = listaMutable.count() == 0
+
 }
 
 fun main(args: Array<String>) {
-    fun <T> reverse(lista: List<T>):Pila<T>{
+    fun <T> reverse(lista: List<T>):List<T>{
         var p1= Pila(lista)
         var pc2: MutableList<T> = mutableListOf()
         var np2=pc2.toList()
         var p2= Pila(np2)
         var elemento: T
+
         if(!p1.vacia()){
-            for (item in p1.get()){
+            for (item in lista){
                 elemento= p1.tope()!!
                 p1.pop()
                 p2.push(elemento)
 
             }
         }
-        return p2
+        return p2.get()
     }
     var numbers = listOf("one", "two", "three", "four")
     var numbersRev = reverse(numbers)
